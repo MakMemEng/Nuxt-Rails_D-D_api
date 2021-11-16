a1acd0791025f9097d5b7a89b8b6937662cde74c
makmemeng <yatani.mems4568@gmail.com>
2021-11-16 16:09:45 +0900

    add_user_model_test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,8 +16,12 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  def active_user
+    User.find_by(activated: true)
+  end
+
+  # # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
